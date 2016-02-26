b62386c73c84ff06ac4161e5ae1695e3819cff84
xiong wenping <wp.xiong@gmail.com>
2016-02-26 14:43:38 +0900

    commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "./log"
+  "./process"
+  "runtime"
+  "time"
+)
+
+func processJob(interface{}) interface{} {
+   time.Sleep(20)
+   return 5
+}
+
+func AddJob(){
+   for i:=1; i< 1000000; i++ {
+      workjob := &process.WorkJob{}
+      workjob.WorkProcess = processJob
+      process.AddJob(workjob)
+   }
+}
+
+func main() {
+   log.InitLog()
+   runtime.GOMAXPROCS(runtime.NumCPU())
+   process.New()
+   AddJob()
+   process.StopWork()
+}
